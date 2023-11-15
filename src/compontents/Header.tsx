db327a3252d7a177fb5dac4301fd19d2238a0ba1
Sven <svenrisse0@gmail.com>
2023-11-15 15:26:08 +0100

    Header for lg+

--- src/compontents/Header.tsx
+++ src/compontents/Header.tsx
@@ -9,8 +9,8 @@ export default function Header({
   active: "home" | "archive" | "about";
 }) {
   return (
-    <header className="border-b">
-      <div className="flex">
+    <header className="flex flex-col border-b">
+      <div className="flex lg:justify-between">
         <div className="flex items-center gap-3 px-2 py-2 md:px-6 md:py-4">
           <Avatar>
             <AvatarImage src="https://github.com/shadcn.png" />
@@ -18,19 +18,51 @@ export default function Header({
           </Avatar>
           <h2 className="font-bold md:text-2xl">Weltpinion</h2>
         </div>
-        <div className="ml-auto flex items-center md:gap-4 md:px-6">
-          <Button className="bg-teal-600">
+        <nav className="hidden justify-evenly px-2 pt-2 md:pt-0 lg:flex lg:w-1/3 lg:self-end">
+          <Link
+            href={"/"}
+            className={`${
+              active === "home" && "border-primary rounded-none border-b-2"
+            } w-1/3`}
+          >
+            <Button variant={"ghost"} className="w-full">
+              Home
+            </Button>
+          </Link>
+          <Link
+            href={"/archive"}
+            className={`${
+              active === "archive" && "border-primary rounded-none border-b-2"
+            } w-1/3`}
+          >
+            <Button variant={"ghost"} className="w-full">
+              Archive
+            </Button>
+          </Link>
+          <Link
+            href={"/about"}
+            className={`${
+              active === "about" && "border-primary rounded-none border-b-2"
+            } w-1/3`}
+          >
+            <Button variant={"ghost"} className="w-full">
+              About
+            </Button>
+          </Link>
+        </nav>
+        <div className="ml-auto flex items-center md:gap-4 md:px-6 lg:ml-0">
+          <Button>
             <Plus className="mr-2 h-4 w-4" />
             Subscribe
           </Button>
           <Button variant={"ghost"}>Sign in</Button>
         </div>
       </div>
-      <nav className="flex w-full justify-evenly px-2 pt-2 md:pt-0">
+      <nav className="flex justify-evenly px-2 pt-2 md:pt-0 lg:hidden">
         <Link
           href={"/"}
           className={`${
-            active === "home" && "rounded-none border-b-2 border-teal-600"
+            active === "home" && "border-primary rounded-none border-b-2"
           } w-1/3`}
         >
           <Button variant={"ghost"} className="w-full">
@@ -40,7 +72,7 @@ export default function Header({
         <Link
           href={"/archive"}
           className={`${
-            active === "archive" && "rounded-none border-b-2 border-teal-600"
+            active === "archive" && "border-primary rounded-none border-b-2"
           } w-1/3`}
         >
           <Button variant={"ghost"} className="w-full">
@@ -50,7 +82,7 @@ export default function Header({
         <Link
           href={"/about"}
           className={`${
-            active === "about" && "rounded-none border-b-2 border-teal-600"
+            active === "about" && "border-primary rounded-none border-b-2"
           } w-1/3`}
         >
           <Button variant={"ghost"} className="w-full">
