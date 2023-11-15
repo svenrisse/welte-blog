b29acb73c0e3de7e7267d259986bbbf75399f5f3
Sven <svenrisse0@gmail.com>
2023-11-15 21:18:05 +0100

    add PostList component

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,29 +1,8 @@
 import Head from "next/head";
-import { useEffect, useState } from "react";
-import client from "tina/__generated__/client";
-import {
-  type PostConnectionEdges,
-  type PostConnectionQuery,
-} from "tina/__generated__/types";
 import Header from "~/compontents/Header";
-import PostPreview from "~/compontents/PostPreview";
+import PostList from "~/compontents/PostList";
 
 export default function Home() {
-  const [data, setData] = useState<PostConnectionQuery>();
-
-  useEffect(() => {
-    async function getPosts() {
-      const postsResponse = await client.queries.postConnection();
-      setData(postsResponse.data);
-    }
-    void getPosts();
-  }, []);
-
-  const posts = data?.postConnection.edges?.map((post) => {
-    return (
-      <PostPreview key={post?.node?.id} post={post as PostConnectionEdges} />
-    );
-  });
   return (
     <>
       <Head>
@@ -33,8 +12,8 @@ export default function Home() {
       </Head>
       <Header active="home" />
       <main className="flex flex-col items-center justify-center">
-        <div className="flex w-full flex-col items-center justify-center gap-12 px-4 py-16 ">
-          {posts}
+        <div className="w-full px-6 py-6">
+          <PostList />
         </div>
       </main>
     </>
