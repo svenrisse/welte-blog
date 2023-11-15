fa1206421e175867ee366e0ec8ae0850967a6433
Sven <svenrisse0@gmail.com>
2023-11-15 17:30:51 +0100

    fix header active

--- src/pages/blog/[page].tsx
+++ src/pages/blog/[page].tsx
@@ -23,7 +23,7 @@ export default function Page() {
 
   return (
     <>
-      <Header active="about" />
+      <Header active="archive" />
       <main className="flex flex-col items-center justify-center">
         <div className="container flex flex-col items-center justify-center gap-12 px-4 py-16 ">
           <div className="prose">
