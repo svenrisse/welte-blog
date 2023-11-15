8ec5177dfb9f9f3d22ac76b2e4318d58adaa66a9
Sven <svenrisse0@gmail.com>
2023-11-15 15:04:08 +0100

    remove wrapping div and Header for md

--- src/compontents/Header.tsx
+++ src/compontents/Header.tsx
@@ -11,14 +11,14 @@ export default function Header({
   return (
     <header className="border-b">
       <div className="flex">
-        <div className="flex items-center gap-3 px-2 py-2">
+        <div className="flex items-center gap-3 px-2 py-2 md:px-6 md:py-4">
           <Avatar>
             <AvatarImage src="https://github.com/shadcn.png" />
             <AvatarFallback>CN</AvatarFallback>
           </Avatar>
-          <h2 className="font-bold">Weltpinion</h2>
+          <h2 className="font-bold md:text-2xl">Weltpinion</h2>
         </div>
-        <div className="ml-auto flex items-center">
+        <div className="ml-auto flex items-center md:gap-4 md:px-6">
           <Button className="bg-teal-600">
             <Plus className="mr-2 h-4 w-4" />
             Subscribe
@@ -26,40 +26,38 @@ export default function Header({
           <Button variant={"ghost"}>Sign in</Button>
         </div>
       </div>
-      <div>
-        <nav className="flex w-full justify-evenly px-2 pt-2">
-          <Link
-            href={"/"}
-            className={`${
-              active === "home" && "rounded-none border-b-2 border-teal-600"
-            } w-1/3`}
-          >
-            <Button variant={"ghost"} className="w-full">
-              Home
-            </Button>
-          </Link>
-          <Link
-            href={"/archive"}
-            className={`${
-              active === "archive" && "rounded-none border-b-2 border-teal-600"
-            } w-1/3`}
-          >
-            <Button variant={"ghost"} className="w-full">
-              Archive
-            </Button>
-          </Link>
-          <Link
-            href={"/about"}
-            className={`${
-              active === "about" && "rounded-none border-b-2 border-teal-600"
-            } w-1/3`}
-          >
-            <Button variant={"ghost"} className="w-full">
-              About
-            </Button>
-          </Link>
-        </nav>
-      </div>
+      <nav className="flex w-full justify-evenly px-2 pt-2 md:pt-0">
+        <Link
+          href={"/"}
+          className={`${
+            active === "home" && "rounded-none border-b-2 border-teal-600"
+          } w-1/3`}
+        >
+          <Button variant={"ghost"} className="w-full">
+            Home
+          </Button>
+        </Link>
+        <Link
+          href={"/archive"}
+          className={`${
+            active === "archive" && "rounded-none border-b-2 border-teal-600"
+          } w-1/3`}
+        >
+          <Button variant={"ghost"} className="w-full">
+            Archive
+          </Button>
+        </Link>
+        <Link
+          href={"/about"}
+          className={`${
+            active === "about" && "rounded-none border-b-2 border-teal-600"
+          } w-1/3`}
+        >
+          <Button variant={"ghost"} className="w-full">
+            About
+          </Button>
+        </Link>
+      </nav>
     </header>
   );
 }
