e631d9b3b0c5edda6f8737e20f294404ccf35328
Sven <svenrisse0@gmail.com>
2023-11-15 20:36:50 +0100

    fix types and styling

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,27 +2,24 @@ import Head from "next/head";
 import { useEffect, useState } from "react";
 import client from "tina/__generated__/client";
 import {
-  PostConnectionEdges,
+  type PostConnectionEdges,
   type PostConnectionQuery,
 } from "tina/__generated__/types";
 import Header from "~/compontents/Header";
-import { type Query } from "~/types/tina";
 import PostPreview from "~/compontents/PostPreview";
 
 export default function Home() {
-  const [data, setData] = useState<Query<PostConnectionQuery>>();
+  const [data, setData] = useState<PostConnectionQuery>();
 
   useEffect(() => {
     async function getPosts() {
       const postsResponse = await client.queries.postConnection();
-      setData(postsResponse);
+      setData(postsResponse.data);
     }
     void getPosts();
   }, []);
 
-  console.log(data);
-
-  const posts = data?.data.postConnection.edges?.map((post) => {
+  const posts = data?.postConnection.edges?.map((post) => {
     return (
       <PostPreview key={post?.node?.id} post={post as PostConnectionEdges} />
     );
@@ -36,7 +33,7 @@ export default function Home() {
       </Head>
       <Header active="home" />
       <main className="flex flex-col items-center justify-center">
-        <div className="container flex flex-col items-center justify-center gap-12 px-4 py-16 ">
+        <div className="flex w-full flex-col items-center justify-center gap-12 px-4 py-16 ">
           {posts}
         </div>
       </main>
