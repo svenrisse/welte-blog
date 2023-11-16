c4f089c7c11ed672b9e9cf1413043f0eb61a701e
Sven <svenrisse0@gmail.com>
2023-11-16 15:02:51 +0100

    add recs section

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,7 @@
 import Head from "next/head";
 import Header from "~/components/Header";
 import PostList from "~/components/PostList";
+import { Separator } from "~/components/ui/separator";
 
 export default function Home() {
   return (
@@ -14,6 +15,13 @@ export default function Home() {
       <main className="flex flex-col items-center justify-center">
         <div className="w-full px-6 py-6">
           <PostList />
+        </div>
+        <div>
+          <div>
+            <h2 className="font-bold">Recommendations</h2>
+            <span>View all</span>
+          </div>
+          <Separator />
         </div>
       </main>
     </>
