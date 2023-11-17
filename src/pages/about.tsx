19d1e1da37f4a0a919748d797428cc2b00d596b6
Sven <svenrisse0@gmail.com>
2023-11-17 08:55:14 +0100

    add skeleton to about

--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -2,9 +2,10 @@ import Header from "~/components/Header";
 import client from "tina/__generated__/client";
 import { TinaMarkdown } from "tinacms/dist/rich-text";
 import { useQuery } from "@tanstack/react-query";
+import { Skeleton } from "~/components/ui/skeleton";
 
 export default function About() {
-  const { data } = useQuery({
+  const { data, isInitialLoading } = useQuery({
     queryKey: ["about"],
     refetchOnWindowFocus: false,
     queryFn: async () => {
@@ -14,15 +15,33 @@ export default function About() {
       return about;
     },
   });
+
   return (
     <>
       <Header active="about" />
       <main className="flex flex-col items-center justify-center">
-        <div className="flex flex-col items-center justify-center px-4 py-8">
-          <div className="prose">
-            {/* eslint-disable-next-line @typescript-eslint/no-unsafe-assignment */}
-            <TinaMarkdown content={data?.data.about.body} />
-          </div>
+        <div className="flex w-full flex-col items-center justify-center px-4 py-8">
+          {isInitialLoading ? (
+            <div className="w-full py-8">
+              <Skeleton className="h-10 w-10/12 rounded-full" />
+              <div className="flex flex-col gap-4 py-8">
+                <Skeleton className="h-6" />
+                <Skeleton className="h-6" />
+                <Skeleton className="h-6 w-8/12" />
+                <Skeleton className="h-6" />
+                <Skeleton className="h-6" />
+                <Skeleton className="h-6" />
+                <Skeleton className="h-6" />
+                <Skeleton className="h-6" />
+                <Skeleton className="h-6 w-4/12" />
+              </div>
+            </div>
+          ) : (
+            <div className="prose">
+              {/* eslint-disable-next-line @typescript-eslint/no-unsafe-assignment */}
+              <TinaMarkdown content={data?.data.about.body} />
+            </div>
+          )}
         </div>
       </main>
     </>
