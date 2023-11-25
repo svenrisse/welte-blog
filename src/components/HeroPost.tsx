363c0b44af9bf17a2db36341e8e2f6a3f3ced4d1
Sven <svenrisse0@gmail.com>
2023-11-25 18:18:15 +0100

    hero for md

--- src/components/HeroPost.tsx
+++ src/components/HeroPost.tsx
@@ -14,30 +14,32 @@ export default function HeroPost({ post }: { post: PostConnectionEdges }) {
   );
 
   return (
-    <div className="w-full">
-      <Link href={`/blog/${post?.node?._sys.filename}`}>
-        <div>
-          <Image
-            src={post.node!.heroImage}
-            alt={post.node!.title}
-            width={0}
-            height={0}
-            sizes="100vw"
-            className="h-60 w-full"
-          />
+    <Link href={`/blog/${post?.node?._sys.filename}`}>
+      <div className="flex w-full flex-col md:flex-row md:items-center md:px-8 md:pb-2 md:pt-4">
+        <Image
+          src={post.node!.heroImage}
+          alt={post.node!.title}
+          width={0}
+          height={0}
+          sizes="100vw"
+          className="h-60 w-full md:h-full md:w-6/12 md:rounded-xl"
+        />
+        <div className="md:h-full md:max-h-full md:w-6/12">
+          <div className="flex flex-col px-6 pt-4">
+            <h2 className="text-2xl font-semibold">{post?.node?.title}</h2>
+            <p className="pb-2 pt-4 text-sm text-gray-500">
+              {post?.node?.description}
+            </p>
+            <span className="text-sm text-gray-500">{formattedDate}</span>
+          </div>
+          <div className="flex gap-2 px-6 pb-1 md:pt-2">
+            <PostBadges post={post} />
+          </div>
+          <div className="px-6 pt-4 md:pt-0">
+            <PostActions postName={post.node!._sys.filename} />
+          </div>
         </div>
-        <div className="flex flex-col gap-4 px-6 pt-4">
-          <h2 className="text-2xl font-semibold">{post?.node?.title}</h2>
-          <p className="text-sm text-gray-500">{post?.node?.description}</p>
-          <span className="text-sm text-gray-500">{formattedDate}</span>
-        </div>
-      </Link>
-      <div className="flex gap-2 px-6 pb-1 pt-4">
-        <PostBadges post={post} />
       </div>
-      <div className="px-6 pt-4">
-        <PostActions postName={post.node!._sys.filename} />
-      </div>
-    </div>
+    </Link>
   );
 }
