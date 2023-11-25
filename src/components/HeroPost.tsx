ce58d2e7d1b0861928412d126b6488734dfc9aa3
Sven <svenrisse0@gmail.com>
2023-11-25 20:43:48 +0100

    adjust paddings for lg

--- src/components/HeroPost.tsx
+++ src/components/HeroPost.tsx
@@ -26,15 +26,15 @@ export default function HeroPost({ post }: { post: PostConnectionEdges }) {
         />
         <div className="flex flex-col md:w-6/12 md:items-center md:justify-center lg:px-8">
           <div className="flex flex-col px-6 pt-4 md:pt-2 md:text-center">
-            <h2 className="text-2xl font-semibold lg:text-3xl">
+            <h2 className="text-2xl font-semibold lg:pb-2 lg:text-3xl">
               {post?.node?.title}
             </h2>
-            <p className="py-4 text-sm text-gray-500 md:py-2 lg:text-base">
+            <p className="py-4 text-sm text-gray-500 md:py-2 lg:pb-4 lg:text-base">
               {post?.node?.description}
             </p>
             <span className="text-sm text-gray-500">{formattedDate}</span>
           </div>
-          <div className="flex gap-2 px-6 py-4 md:py-2">
+          <div className="flex gap-2 px-6 py-4 md:py-2 lg:py-4">
             <PostBadges post={post} />
           </div>
           <div className="flex justify-between px-6 md:justify-center md:gap-8">
