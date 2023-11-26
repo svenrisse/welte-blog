bb9a0d63bab0740dea0c7d393e9e0fdca04bce82
Sven <svenrisse0@gmail.com>
2023-11-26 14:57:20 +0100

    add TypographyQuote

---
+++ src/components/Typography/TypographyQuote.tsx
@@ -0,0 +1,9 @@
+export function TypographyBlockquote({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <blockquote className="mt-6 border-l-2 pl-6 italic">{children}</blockquote>
+  );
+}
