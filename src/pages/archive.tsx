41cd71584ad400e4c74d120aeaadeb31f38060ea
Sven <svenrisse0@gmail.com>
2023-11-26 09:29:40 +0100

    render posts in archive

--- src/pages/archive.tsx
+++ src/pages/archive.tsx
@@ -1,14 +1,41 @@
 import Header from "~/components/Header";
-import PostList from "~/components/PostList";
+import { useQuery } from "@tanstack/react-query";
+import client from "tina/__generated__/client";
+import { Separator } from "~/components/ui/separator";
+import PostPreview from "~/components/PostPreview";
+import { PostConnectionEdges } from "tina/__generated__/types";
 
 export default function Archive() {
+  const { data, isInitialLoading } = useQuery({
+    queryKey: ["posts"],
+    refetchOnWindowFocus: false,
+    queryFn: async () => {
+      const postsResponse = await client.queries.postConnection({});
+      return postsResponse;
+    },
+  });
+
+  const sortedEdges = data?.data?.postConnection?.edges?.sort(function (a, b) {
+    if (a!.node!.createdAt! < b!.node!.createdAt!) return 1;
+    if (a!.node!.createdAt! > b!.node!.createdAt!) return -1;
+    return 0;
+  });
+
+  const posts = sortedEdges?.map((post, index) => {
+    return (
+      <>
+        <Separator />
+        <PostPreview key={post?.node?.id} post={post as PostConnectionEdges} />
+        {index + 1 === sortedEdges.length && <Separator />}
+      </>
+    );
+  });
+
   return (
     <>
       <Header active="archive" />
       <main className="flex flex-col items-center justify-center">
-        <div className="w-full">
-          <PostList />
-        </div>
+        <div>{posts}</div>
       </main>
     </>
   );
