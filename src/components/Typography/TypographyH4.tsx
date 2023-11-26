5cce01071c7ba6d9b8af7396a12067711c70f4c5
Sven <svenrisse0@gmail.com>
2023-11-26 17:04:46 +0100

    add TypographyH4

---
+++ src/components/Typography/TypographyH4.tsx
@@ -0,0 +1,7 @@
+export function TypographyH4({ children }: { children: React.ReactNode }) {
+  return (
+    <h4 className="scroll-m-20 text-sm font-semibold  tracking-tight md:text-lg">
+      {children}
+    </h4>
+  );
+}
