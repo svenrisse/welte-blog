cb568d28ba38dd71ec03efa638719fc3a03d54a0
Sven <svenrisse0@gmail.com>
2023-12-16 15:33:19 +0100

    archive page responsive for all sizes

--- src/pages/archive.tsx
+++ src/pages/archive.tsx
@@ -4,8 +4,11 @@ import client from "tina/__generated__/client";
 import { Separator } from "~/components/ui/separator";
 import PostPreview from "~/components/PostPreview";
 import { PostConnectionEdges } from "tina/__generated__/types";
+import { Button } from "~/components/ui/button";
+import { useState } from "react";
 
 export default function Archive() {
+  const [sort, setSort] = useState<"new" | "top">("new");
   const { data, isInitialLoading } = useQuery({
     queryKey: ["posts"],
     refetchOnWindowFocus: false,
@@ -34,8 +37,22 @@ export default function Archive() {
   return (
     <>
       <Header active="archive" />
-      <main className="flex flex-col items-center justify-center">
-        <div>{posts}</div>
+      <main className="mx-auto flex w-full flex-col items-center justify-center md:w-10/12 lg:w-5/12 xl:w-4/12 2xl:w-3/12">
+        <div className="flex gap-4 self-start px-6 py-2">
+          <Button
+            variant={`${sort === "new" ? "secondary" : "ghost"}`}
+            onClick={() => setSort("new")}
+          >
+            New
+          </Button>
+          <Button
+            variant={`${sort === "top" ? "secondary" : "ghost"}`}
+            onClick={() => setSort("top")}
+          >
+            Top
+          </Button>
+        </div>
+        <div className="w-full">{posts}</div>
       </main>
     </>
   );
