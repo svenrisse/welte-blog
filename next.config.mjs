e28e363f8a68c1a12ddabf94abdb80dadac45311
Sven <svenrisse0@gmail.com>
2023-12-22 22:52:08 +0100

    add discord image provider

--- next.config.mjs
+++ next.config.mjs
@@ -7,6 +7,7 @@ await import("./src/env.mjs");
 /** @type {import("next").NextConfig} */
 const config = {
   images: {
+    domains: ["cdn.discordapp.com"],
     remotePatterns: [
       {
         protocol: "https",
