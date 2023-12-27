80a811ba182e8d82b6c623ff305d9dc8ec276a91
Sven <svenrisse0@gmail.com>
2023-12-27 22:17:27 +0100

    refactor(ShareButton): move into own component, drawer for mobile, popup for screens

---
+++ src/components/ShareButton.tsx
@@ -0,0 +1,39 @@
+import {
+  Drawer,
+  DrawerClose,
+  DrawerContent,
+  DrawerDescription,
+  DrawerFooter,
+  DrawerHeader,
+  DrawerTitle,
+  DrawerTrigger,
+} from "../components/ui/drawer";
+import { Button } from "./ui/button";
+import { Share } from "lucide-react";
+
+export const ShareButton = () => {
+  return (
+    <Drawer>
+      <DrawerTrigger asChild>
+        <Button variant={"ghost"} size={"sm"}>
+          <Share />
+        </Button>
+      </DrawerTrigger>
+      <DrawerContent>
+        <div className="mx-auto w-full max-w-sm">
+          <DrawerHeader>
+            <DrawerTitle>Move Goal</DrawerTitle>
+            <DrawerDescription>Set your daily activity goal.</DrawerDescription>
+          </DrawerHeader>
+          <div className="p-4 pb-0"></div>
+          <DrawerFooter>
+            <Button>Submit</Button>
+            <DrawerClose asChild>
+              <Button variant="outline">Cancel</Button>
+            </DrawerClose>
+          </DrawerFooter>
+        </div>
+      </DrawerContent>
+    </Drawer>
+  );
+};
