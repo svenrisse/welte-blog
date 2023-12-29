441bac01162c2550e28707b2e74f928491a05812
Sven <svenrisse0@gmail.com>
2023-12-29 15:01:19 +0100

    feat(Comment): add responses

--- src/components/Comment.tsx
+++ src/components/Comment.tsx
@@ -9,29 +9,32 @@ import { CommentDropdown } from "./CommentDropdown";
 import CommentActions from "./CommentActions";
 import { type Session } from "next-auth";
 
+type Comment = {
+  user: {
+    id: string;
+    name: string;
+    email: string | null;
+    emailVerified: Date | null;
+    image: string | null;
+  };
+  id: string;
+  postId: string;
+  userId: string;
+  text: string;
+  createdAt: Date;
+  updatedAt: Date;
+  originalCommentId: string | null;
+  Responses: Comment[];
+  _count: {
+    CommentLikes: number;
+    Responses: number;
+  };
+};
+
 type PostCommentsProps = {
   postName: string;
   session: Session | null;
-  comment: {
-    user: {
-      id: string;
-      name: string;
-      email: string | null;
-      emailVerified: Date | null;
-      image: string | null;
-    };
-    id: string;
-    postId: string;
-    userId: string;
-    text: string;
-    createdAt: Date;
-    updatedAt: Date;
-    originalCommentId: string | null;
-    _count: {
-      CommentLikes: number;
-      Responses: number;
-    };
-  };
+  comment: Comment;
 };
 
 export const Comment = ({ comment, postName, session }: PostCommentsProps) => {
@@ -41,8 +44,19 @@ export const Comment = ({ comment, postName, session }: PostCommentsProps) => {
     "MMM d" + (date.getFullYear() == new Date().getFullYear() ? "" : ", YYYY"),
   );
 
+  const responses = comment.Responses?.map((response) => {
+    return (
+      <Comment
+        key={response.id}
+        postName={postName}
+        session={session}
+        comment={response}
+      />
+    );
+  });
+
   return (
-    <div key={comment.id} className="flex gap-4 py-6 first:pt-0">
+    <div key={comment.id} className="flex gap-4 first:pt-0">
       <div className="flex flex-col items-center gap-2">
         <Avatar>
           {comment.user.image ? (
@@ -56,13 +70,21 @@ export const Comment = ({ comment, postName, session }: PostCommentsProps) => {
             </AvatarFallback>
           )}
         </Avatar>
-        <Separator orientation="vertical" />
+        <div className="h-[100%] w-px bg-muted"></div>
       </div>
-      <div className="flex flex-col gap-6">
+      <div className="flex w-full flex-col gap-4">
         <div className="flex flex-col gap-2">
           <div className="flex gap-4">
             <Typography>{comment.user.name}</Typography>
             <TypographyMuted>{formattedDate}</TypographyMuted>
+            <div className="ml-auto">
+              <CommentDropdown
+                userIsOrig={session?.user.id === comment.userId}
+                userRole={session?.user.role}
+                commentId={comment.id}
+                postName={postName}
+              />
+            </div>
           </div>
           <TypographySmall>{comment.text}</TypographySmall>
         </div>
@@ -74,15 +96,7 @@ export const Comment = ({ comment, postName, session }: PostCommentsProps) => {
             count={comment._count}
           />
         </div>
-        <div>{/* Responses          */}</div>
-      </div>
-      <div className="ml-auto">
-        <CommentDropdown
-          userIsOrig={session?.user.id === comment.userId}
-          userRole={session?.user.role}
-          commentId={comment.id}
-          postName={postName}
-        />
+        <div className="flex w-full flex-col gap-4">{responses}</div>
       </div>
     </div>
   );
