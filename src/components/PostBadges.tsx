c1b65360f55fb632a3d820d9068ef7aef26d9d70
Sven <svenrisse0@gmail.com>
2024-01-02 15:36:34 +0100

    refactor(PostBadges): post -> tags prop

--- src/components/PostBadges.tsx
+++ src/components/PostBadges.tsx
@@ -1,27 +1,27 @@
 import Link from "next/link";
 import { badgeVariants } from "./ui/badge";
-import { type PostConnectionEdges } from "tina/__generated__/types";
 
-export default function PostBadges({ post }: { post: PostConnectionEdges }) {
+export default function PostBadges({
+  tags,
+}: {
+  tags: (string | null)[] | null | undefined;
+}) {
   return (
     <>
-      {post.node?.tags?.slice(0, 2).map((tag) => {
+      {tags?.slice(0, 2).map((tag) => {
         return (
           <Link
             className={badgeVariants({ variant: "secondary" })}
             href={"/"}
-            key={post.node?.id}
+            key={crypto.randomUUID()}
           >
             {tag}
           </Link>
         );
       })}
-      <Link
-        className={badgeVariants({ variant: "outline" })}
-        href={`/blog/${post?.node?._sys.filename}`}
-      >
-        {post.node?.tags?.slice(2).length ? (
-          <>{post.node.tags.slice(2).length} more</>
+      <Link className={badgeVariants({ variant: "outline" })} href={"/"}>
+        {tags?.slice(2).length ? (
+          <>{tags.slice(2).length} more</>
         ) : (
           <>No tags</>
         )}
